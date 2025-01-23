0974915a74a732cf701ab0b599ea70bb6b2cc3b5
Craziiivan <luckybn310@gmail.com>
2025-01-23 11:49:18 +0630

    integrate with nextjs

--- lib/auth.ts
+++ lib/auth.ts
@@ -5,5 +5,8 @@ import { db } from "@/db";
 export const auth = betterAuth({
     database: drizzleAdapter(db, {
         provider: "pg",
-    })
+    }),
+    emailAndPassword: {
+        enabled: true
+    }
 });
