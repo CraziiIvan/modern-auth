1891376e5dc98debb347ebf5d21b6c203230a3a1
Craziiivan <luckybn310@gmail.com>
2025-03-02 00:33:03 +0630

    fix lint error

--- components/form/LoginForm.tsx
+++ components/form/LoginForm.tsx
@@ -158,7 +158,7 @@ export default function LoginForm() {
         </Button>
       </div>
       <div className="text-center text-sm text-muted-foreground">
-        Don't have an account?{" "}
+        Don&apos;t have an account?{" "}
         <a href="/signup" className="underline underline-offset-4">
           Sign up
         </a>
